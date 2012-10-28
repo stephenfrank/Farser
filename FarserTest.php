5c27d14e5be9ac7d345044a101190f976f5edcff
Stephen Frank <stephen@flowsa.com>
2012-10-28 11:28:58 +0200

    tweaks and readme

--- FarserTest.php
+++ FarserTest.php
@@ -6,37 +6,37 @@ class Farser_Test extends PHPUnit_Framework_TestCase {
 
 	public function testSimple()
 	{
-		$lexer = new Farser();
-		$lexer->setRaw('-- {$A}foo{/$A} --');
-		$this->assertEquals('-- foo --', $lexer->parse());
+		$farser = new Farser();
+		$farser->setRaw('-- {$A}foo{/$A} --');
+		$this->assertEquals('-- foo --', $farser->parse());
 	}
 
 	public function testSimpleMultiline()
 	{
-		$lexer = new Farser();
-		$lexer->setRaw("{\$A}\n\tfoo\n{/\$A}-");
-		$this->assertEquals("\n\tfoo\n-", $lexer->parse());
+		$farser = new Farser();
+		$farser->setRaw("{\$A}\n\tfoo\n{/\$A}-");
+		$this->assertEquals("\n\tfoo\n-", $farser->parse());
 	}
 
 	public function testSimpleNested()
 	{
-		$lexer = new Farser();
-		$lexer->setRaw('{$A}foo{$B}bar{/$B}{/$A}');
-		$this->assertEquals('foobar', $lexer->parse());
+		$farser = new Farser();
+		$farser->setRaw('{$A}foo{$B}bar{/$B}{/$A}');
+		$this->assertEquals('foobar', $farser->parse());
 	}
 
 	public function testSimpleNestedVars()
 	{
-		$lexer = new Farser();
-		$lexer->setRaw('{$A fiz="baz"}foo{$B}bar{fiz}{/$B}{/$A}');
-		$this->assertEquals('foobarbaz', $lexer->parse());
+		$farser = new Farser();
+		$farser->setRaw('{$A fiz="baz"}foo{$B}bar{fiz}{/$B}{/$A}');
+		$this->assertEquals('foobarbaz', $farser->parse());
 	}
 
 	public function testSimpleNestedVarsScoped()
 	{
-		$lexer = new Farser();
-		$lexer->setRaw('{$A fiz="baz" dir="ls"}foo{dir}{$B dir="dir"}bar{fiz}{dir}{/$B}{/$A}');
-		$this->assertEquals('foolsbarbazdir', $lexer->parse());
+		$farser = new Farser();
+		$farser->setRaw('{$A fiz="baz" dir="ls"}foo{dir}{$B dir="dir"}bar{fiz}{dir}{/$B}{/$A}');
+		$this->assertEquals('foolsbarbazdir', $farser->parse());
 	}
 
 	/**
@@ -44,40 +44,40 @@ class Farser_Test extends PHPUnit_Framework_TestCase {
 	 */
 	public function testNoClosingTagException()
 	{
-		$lexer = new Farser();
-		$lexer->setRaw('{$A}{foo}{$A}');
-		$lexer->parse();
+		$farser = new Farser();
+		$farser->setRaw('{$A}{foo}{$A}');
+		$farser->parse();
 	}
 
 	public function testSimpleCallback()
 	{
-		$lexer = new Farser();
-		$lexer->setRaw('{$A}{foo}{/$A}');
+		$farser = new Farser();
+		$farser->setRaw('{$A}{foo}{/$A}');
 		
-		$lexer->addCallback('A', function (& $scope)
+		$farser->addCallback('A', function (& $scope)
 		{
 			$scope['vars']['foo'] = 'baz';
 		});
 
-		$this->assertEquals('baz', $lexer->parse());
+		$this->assertEquals('baz', $farser->parse());
 	}
 
 	public function testSimpleNestedCallback()
 	{
-		$lexer = new Farser();
-		$lexer->setRaw('{$A}{foo}{$B}{foo}{dir}{/$B}{/$A}');
+		$farser = new Farser();
+		$farser->setRaw('{$A}{foo}{$B}{foo}{dir}{/$B}{/$A}');
 		
-		$lexer->addCallback('A', function (& $scope)
+		$farser->addCallback('A', function (& $scope)
 		{
 			$scope['vars']['foo'] = 'baz';
 		});
 
-		$lexer->addCallback('B', function (& $scope)
+		$farser->addCallback('B', function (& $scope)
 		{
 			$scope['vars']['dir'] = 'ls';
 		});
 
-		$this->assertEquals('bazbazls', $lexer->parse());
+		$this->assertEquals('bazbazls', $farser->parse());
 	}
 
 }
---
+++ README.md
@@ -0,0 +1,34 @@
+Farser
+======
+
+An experiment in parsing (in PHP) that's pretty simple minded 
+and tries to implement nested scopes in a non-scary way.
+
+And... look ma, no regex!
+
+Turns
+
+    {$A fiz="baz" dir="ls"}foo{dir}{$B dir="dir"}bar{fiz}{dir}{/$B}{/$A}
+
+into
+
+    foolsbarbazdir
+
+also handles a simple callback mechanism
+
+    $farser = new Farser();
+    $farser->setRaw('{$A}{foo}{$B}{foo}{dir}{/$B}{/$A}');
+
+    $farser->addCallback('A', function (& $scope)
+    {
+        $scope['vars']['foo'] = 'baz';
+    });
+
+    $farser->addCallback('B', function (& $scope)
+    {
+        $scope['vars']['dir'] = 'ls';
+    });
+
+    echo $farser->parse();
+
+    // output: bazbazls
